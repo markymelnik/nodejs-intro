c417a2b1547ad785f7687c9ef14afc9d22e1d07b
Mark Melnik <markemelnik@gmail.com>
2023-05-26 14:58:00 -0400

    performing a GET request

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
-require('dotenv').config();
+const https = require('https');
 
-console.log(process.env.USER_ID);
-console.log(process.env.USER_KEY);
-console.log(process.env.NODE_ENV);
+const options = {
+  hostname: 'example.com',
+  port: 443,
+  path: '/todos',
+  method: 'GET',
+};
+
+const req = https.request(options, res => {
+  console.log(`statusCode: ${res.statusCode}`);
+
+  res.on('data', d => {
+    process.stdout.write(d);
+  });
+});
+
+req.on('error', error => {
+  console.error(error);
+});
+
+req.end();
